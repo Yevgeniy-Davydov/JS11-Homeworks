2de5d3857f3845a9f5f7b8cba6777810dfb19f76
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-02-14 22:11:17 +0100

    files added

---
+++ HW6/js/script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+// // 1 Task
+
+// function iterativeOddSumTo(number) {
+
+//     let result = 0;
+
+//     for (let i = 1; i <= number; i += 2){
+        
+//         result = result + i;
+//     }
+//     return result
+    
+//     };
+    
+//     console.log(iterativeOddSumTo(1)); 
+//     console.log(iterativeOddSumTo(9)); 
+//     console.log(iterativeOddSumTo(10)); 
+
+
+
+// // 2 Task
+
+// function recursiveOddSumTo(number) {
+
+//     if (number === 1){
+//         return number
+//     }
+//     if (number % 2 === 1){
+//         return number + recursiveOddSumTo(number - 2)
+//     }
+//         return recursiveOddSumTo(number-1)
+//     };
+    
+//     console.log(recursiveOddSumTo(1));
+//     console.log(recursiveOddSumTo(9));
+//     console.log(recursiveOddSumTo(10)); 
+
+
+

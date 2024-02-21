cef681cb21ae73f8559f4f7d513d07ac27f832e2
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-02-21 17:54:42 +0100

    homework changed

--- HW7/js/scripr.js
+++ HW7/js/scripr.js
@@ -2,129 +2,96 @@
 
 // 1 Task
 
-// const resultsArray = [1, 2, [3, [4]]];
-// let productOfArray = 1;
+const resultsArray = [1, 2, [3, [4]]];
 
-// resultsArray.flat(Infinity).forEach((number) => {
-//     return productOfArray *= number
-//  });
- 
+let productOfArray = resultsArray.flat(Infinity).reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 
-// console.log(productOfArray);
 
-// // 2 Task
+console.log(productOfArray);
 
-// const priceData = {
-//     Apples: '23.4',
-//     BANANAS: '48',
-//     oRAngGEs: '48.7584',
-//     };
+
+// 2 Task
+
+const priceData = {
+    Apples: '23.4',
+    BANANAS: '48',
+    oRAngGEs: '48.7584',
+    };
     
-//     function optimizer(data) {
-
-//         let array = Object.entries(data);
-
-//         let mappedArray = array.map((item) => {
-//             return [item[0].toLowerCase(), Number(item[1]).toFixed(2)]
+    function optimizer(data) {
     
-//         });
-    
-//         let newObject = Object.fromEntries(mappedArray)
-    
-//         return newObject;
+        return Object.fromEntries(Object.entries(data).map(([key, value]) => [key.toLowerCase(), Number(value).toFixed(2)]));
      
-//     };
+    };
     
-//     let updatedPriceData = optimizer(priceData);
+    let updatedPriceData = optimizer(priceData);
     
-//     console.log(updatedPriceData)
-
-
-
-// 2 Task - shorter solution
-
-// const priceData = {
-//     Apples: '23.4',
-//     BANANAS: '48',
-//     oRAngGEs: '48.7584',
-//     };
-    
-//     function optimizer(data) {
-    
-//         return Object.fromEntries(Object.entries(data).map((item) => [item[0].toLowerCase(), Number(item[1]).toFixed(2)]));
-     
-//     };
-    
-//     let updatedPriceData = optimizer(priceData);
-    
-//     console.log(updatedPriceData)
+    console.log(updatedPriceData)
 
 
 
 // 3 Task - methods of array
 
 
-// const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
-// let vowels = ["А", "Е", "Є", "І", "Ї", "О", "У", "Ю", "Я"];
+const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+let vowels = ["А", "Е", "Є", "І", "Ї", "О", "У", "Ю", "Я"];
 
 
-// let filteredNames = [];
-// userNames.forEach((name) =>{
-//    if(vowels.includes(name[0])){
-//       filteredNames.push(name)
-//    }
-//    return filteredNames
-// });
+let filteredNames = [];
+userNames.forEach((name) =>{
+   if(vowels.includes(name[0])){
+      filteredNames.push(name)
+   }
+   return filteredNames
+});
 
-// console.log(filteredNames);
+console.log(filteredNames);
 
 
 // 3 Task - filter
 
-// const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
-// let vowels = ["А", "Е", "Є", "І", "Ї", "О", "У", "Ю", "Я"];
+const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+let vowels = ["А", "Е", "Є", "І", "Ї", "О", "У", "Ю", "Я"];
 
-//  let filteredNames = userNames.filter((name)=> {
-//    return vowels.includes(name[0])
-//  })
+ let filteredNames = userNames.filter((name)=> {
+   return vowels.includes(name[0])
+ })
 
-// console.log(filteredNames);
+console.log(filteredNames);
 
 
 // 4 Task
 
-// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-// let initials;
+const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+let initials;
 
 
-// let mappedArray = userNames.map((name) =>{
-//    let groupedNames = name.split(" ");
+let mappedArray = userNames.map((name) =>{
+   let groupedNames = name.split(" ");
  
+   let separatedNames = groupedNames.map((item) =>{
+   return item[0];
 
-//    let separatedNames = groupedNames.map((item) =>{
-//     let sinlgleName = item.split("");
-//     return sinlgleName[0];
-    
-//    })
-//     let joinedLetters = separatedNames.join(".") + "."
+   })
+    let joinedLetters = separatedNames.join(".") + "."
 
-//     return joinedLetters
+    return joinedLetters
 
 
-//    });
-// initials = mappedArray.sort()
+   });
+initials = mappedArray.sort()
 
 
-// console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 
 // 4 Task - shorter solution
 
-// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
 
-// let initials = userNames.map((name) => name.split(" ").map((item) => item.split("")[0]).join(".") + ".").sort();
+let initials = userNames.map((name) => name.split(" ").map((item) => item[0]).join(".") + ".").sort();
 
 
-// console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 

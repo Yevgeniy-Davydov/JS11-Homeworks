8fba334b44a68c6f6ae2dd4b429dbaa3322f516a
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-03-05 21:31:55 +0100

    difference between day and days addad

--- HW10/js/script.js
+++ HW10/js/script.js
@@ -1,16 +1,11 @@
 "use strict"
 
 
-// 1 Task
+// // 1 Task
 
 function durationBetweenDates(start = "01 Jan 1970", end = "02 Jan 1970", unit = "days"){
 
-    let difference = Math.abs(new Date (end) - new Date (start));
-
-    
-    // let unitName = difference > 1 ? unit : unit.slice(0, unit.length-1);
-
-     
+    let difference = Math.abs(new Date (end) - new Date (start)); 
 
     switch (unit){
 
@@ -20,17 +15,20 @@ function durationBetweenDates(start = "01 Jan 1970", end = "02 Jan 1970", unit =
 
         case "hours" : return `${Math.floor(difference / 3600000)} ${unit}`;
 
-        case "days" :  return `${Math.floor(difference / 86400000)} ${unit}`
-       
+        case "days" :  
+            let amountDays = Math.floor(difference / 86400000);
+            let unitDays = amountDays > 1 ? "days" : "day";
+
+            return `${amountDays} ${unitDays}`;
 
     }
 
 };
 
 
+
 console.log(durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds')); // поверне '86400 seconds'
 console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')); // поверне '362 days'
-
 
 
 // 2 Task
@@ -43,3 +41,6 @@ const filterUnique = (array) => [...new Set(array)];
 
 
 console.log(filterUnique(userNames)); // ['Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+
+
+

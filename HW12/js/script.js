10fc9f56195dbf7698f3c29abb5115c22f235a0c
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-03-10 21:51:25 +0100

    homework 12 added

--- HW12/js/script.js
+++ HW12/js/script.js
@@ -1,1 +1,25 @@
 "use strict"
+
+// // для елементу з текстом 'Навігація по DOM дереву'
+
+let heading = document.getElementById("headerTwo");
+console.log(heading)
+
+// // для першого елементу <section>
+
+let section = document.querySelector(".firstSection");
+let firstElementInSection = section.firstElementChild;
+
+console.log(firstElementInSection)
+
+// // для елементу списку з текстом 'Пункт 5'
+let underordnedList = document.querySelector("ul");
+let lastListItem = underordnedList.lastElementChild;
+let fifthListItem = lastListItem.previousElementSibling;
+console.log(fifthListItem)
+
+
+// // для елементу з класом 'hatredLevelBlock'
+
+let divWithClass = document.querySelector(".hatredLevelBlock");
+console.log(divWithClass)

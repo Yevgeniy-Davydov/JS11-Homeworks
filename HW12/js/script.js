bb3c3a24c223ebb1a305c8aa6c0d93c1d40504cb
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-03-11 13:22:44 +0100

    another solution for the finding of 5th list item

--- HW12/js/script.js
+++ HW12/js/script.js
@@ -1,25 +1,27 @@
 "use strict"
 
-// // для елементу з текстом 'Навігація по DOM дереву'
+// для елементу з текстом 'Навігація по DOM дереву'
 
 let heading = document.getElementById("headerTwo");
 console.log(heading)
 
-// // для першого елементу <section>
+// для першого елементу <section>
 
 let section = document.querySelector(".firstSection");
 let firstElementInSection = section.firstElementChild;
 
 console.log(firstElementInSection)
 
-// // для елементу списку з текстом 'Пункт 5'
-let underordnedList = document.querySelector("ul");
-let lastListItem = underordnedList.lastElementChild;
-let fifthListItem = lastListItem.previousElementSibling;
+
+// для елементу списку з текстом 'Пункт 5'
+let listCollection = document.querySelectorAll("ul li.text");
+let forthListItem = listCollection[1];
+let fifthListItem = forthListItem.nextElementSibling;
 console.log(fifthListItem)
 
 
-// // для елементу з класом 'hatredLevelBlock'
+// для елементу з класом 'hatredLevelBlock'
 
 let divWithClass = document.querySelector(".hatredLevelBlock");
 console.log(divWithClass)
+
